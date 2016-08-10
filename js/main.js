eeadb636f9492fbfbd4e88f55e1ac31a70bb7991
Epeli Ratoto <Ratoto@Epelis-MacBook-Pro.local>
2016-08-10 11:48:22 -0700

    Added comments to main.js

--- js/main.js
+++ js/main.js
@@ -5,26 +5,34 @@
 
 
 function getInfo() {
+	//declare a variable for the name. Creates a "h3" tag and a textnode.
+	//converts the name to texdtnode and appends it to the "h3" tag.
 	var name = document.getElementById("username").value
 	var nameText = document.createTextNode(name)
 	var title = document.createElement("h3")
 	title.appendChild(nameText)
 	
+	//declare a variable for the comment. Creates a "p" tag and a textnode. 
+	//converts the comment to textnode and appends it to the "p" tag.
 	var commentIn = document.getElementById("comment").value
 	var commentText = document.createTextNode(commentIn)
 	var para = document.createElement("p")
 	para.appendChild(commentText)
 
+	//declare a variable for the user input image. Creates a "img" tag and 
+	//sets the attribute for the "img" tag.
 	var newImage = document.getElementById("inputImage").value
 	var display = document.createElement("img")
 	display.setAttribute("src", newImage)
 
+	//date input
 	var d = new Date()
 	var dateLine = document.createElement("p")
 	dateLine.innerHTML = d
 	var dateTitle = document.createElement("h5")
 	dateTitle.appendChild(dateLine)
 
+	//name, comment, img and date is appended to the "li" tag
 	var newItems = document.createElement("li")
 	newItems.className = "createdList"
 	newItems.appendChild(title)
@@ -32,6 +40,7 @@ function getInfo() {
 	newItems.appendChild(display)
 	newItems.appendChild(dateTitle)
 
+	//"li" is appeded to the "ul"
 	document.getElementById("newList").appendChild(newItems)
 }
 
